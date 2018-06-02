77eff48c251e04b65f1d983cbae90495f0cf627c
swapnil shah <swap.shah@tcs.com>
2018-06-02 09:12:36 +0100

    changes for DI

--- Aviva.FizzBuzz/Global.asax.cs
+++ Aviva.FizzBuzz/Global.asax.cs
@@ -1,9 +1,9 @@
-﻿using System.Web.Mvc;
-using System.Web.Optimization;
-using System.Web.Routing;
-
-namespace Aviva.FizzBuzz
+﻿namespace Aviva.FizzBuzz
 {
+    using System.Web.Mvc;
+    using System.Web.Optimization;
+    using System.Web.Routing;
+
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
